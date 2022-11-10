ced3100b17f63596940d8bd24bfe6bc53fb3c99c
Marc Par <marc.par10@gmail.com>
2022-11-10 22:25:04 +0800

    fix mp4 loadig on safari

--- frontend/vault/src/Components/Media/Media.tsx
+++ frontend/vault/src/Components/Media/Media.tsx
@@ -1,15 +1,31 @@
 import styles from "./Media.module.css";
 import { CircleLoader, BounceLoader } from "react-spinners";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { detect } from "detect-browser";
 
 type MediaProps = {
     src: string
     isLoadingSetter?: Dispatch<SetStateAction<boolean>>
 }
 
+
+async function getMediaSafari(src: string): Promise<string> {
+    let result = await fetch(`${src}`, {
+        redirect: 'follow'
+    });
+    let blob = await result.arrayBuffer();
+    let buffer = Buffer.from(blob);
+    let b64 = buffer.toString('base64');
+    let contentType = result.headers.get('Content-Type');
+    return `data:${contentType ?? ""};base64,${b64}`
+    
+}
+
 export default function Media(props: MediaProps) {
+    let browser = detect();
     let ext = props.src.split(".").pop();
     let [isLoading, setIsLoading] = useState<boolean>(true);
+    let [mediaSrc, setMediaSrc] = useState<string>('');
 
     function onLoadHandler() {
         setIsLoading(false);
@@ -18,11 +34,26 @@ export default function Media(props: MediaProps) {
         }
     }
 
+    useEffect(() => {
+        if (mediaSrc !== '') {
+            return;
+        }
+        if (browser && browser.name === 'safari') {
+            getMediaSafari(props.src).then((src) => {
+                setIsLoading(false);
+                setMediaSrc(src);
+            });
+        } else {
+            setMediaSrc(props.src);
+        }
+    });
+    
+    
     switch (ext) {
         case "mp4" || "webp":
             return (
                 <div className={styles.media_container}>
-                    <video src={props.src} autoPlay muted controls={false} loop className={isLoading ? styles.hidden : styles.media} onPlay={() => {onLoadHandler()}}/>
+                    <video src={mediaSrc} autoPlay muted controls={false} loop className={isLoading ? styles.hidden : styles.media} onPlay={() => {onLoadHandler()}}/>
                     <BounceLoader className={styles.loader} loading={isLoading} color={"rgb(0, 98, 190)"} />
                 </div>
             );
@@ -33,7 +64,7 @@ export default function Media(props: MediaProps) {
         default:
             return (
                 <div className={styles.media_container}>
-                    <img src={props.src} className={isLoading ? styles.hidden : styles.media} onLoad={() => {onLoadHandler()}} alt={"nft"} />
+                    <img src={mediaSrc} className={isLoading ? styles.hidden : styles.media} onLoad={() => {onLoadHandler()}} alt={"nft"} />
                     <BounceLoader className={styles.loader} loading={isLoading} color={"rgb(0, 98, 190)"} />
                 </div>
             );
